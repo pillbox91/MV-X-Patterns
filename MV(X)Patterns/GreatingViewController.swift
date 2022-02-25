67051e895d56c5e4555e5f3d5e59f01c0946c751
Andrey Averyanov <averyanov1996@gmail.com>
2022-02-25 13:17:52 +0300

    Commit

--- MV(X)Patterns/GreatingViewController.swift
+++ MV(X)Patterns/GreatingViewController.swift
@@ -9,21 +9,16 @@ import UIKit
 
 class GreatingViewController: UIViewController {
 
+    @IBOutlet var greatingLabel: UILabel!
+    
+    private var person: Person!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        person = Person(name: "Tim", surname: "Cook")
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func showGreatingPressed() {
+        greatingLabel.text = "Hello, \(person.name) \(person.surname)"
     }
-    */
-
 }
--- MV(X)Patterns/Person.swift
+++ MV(X)Patterns/Person.swift
@@ -5,4 +5,7 @@
 //  Created by Андрей Аверьянов on 25.02.2022.
 //
 
-import Foundation
+struct Person {
+    let name: String
+    let surname: String
+}
